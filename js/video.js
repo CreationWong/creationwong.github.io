643e04edc021b1fc707d648920bd9f7e5a4c89e3
Creation·Wong <85749931+CreationWong@users.noreply.github.com>
2022-03-15 00:30:14 +0800

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,28 +1,105 @@
+//视频提交
+function video(url)
+{ 
+  document.getElementById("mp4_src").src=url;
+  document.getElementById("video").load();
+} 
 //------------------------
 //赛马娘标题模板
-function smn(part,part_1)
+function smn_h3(season,part)
 { 
-  document.getElementById("now").innerHTML="赛马娘 Pretty Derby 第"+part+"季 第"+part_1+"集";
+  document.getElementById("now").innerHTML="赛马娘 Pretty Derby 第"+season+"季 第"+part+"集";
 } 
 //------------------------
-//11集
-function smn_2_11()
+//赛马娘季集判断
+function smn(season,part)
 { 
-  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/7b72cbd05285890815430498246/CrHD8QH7oaMA.mp4";
-  document.getElementById("video").load();
-  smn(2,11)
+	switch (season){
+  		case 1:switch (part){
+    case 1:video("") 
+smn_h3(season,part)
+        break;
+    case 2:video("") 
+smn_h3(season,part)
+        break;
+    case 3:video("") 
+smn_h3(season,part)
+   	 break;
+    case 4:video("") 
+smn_h3(season,part)
+    	break;
+    case 5:video("") 
+smn_h3(season,part)
+        break;
+    case 6:video("") 
+smn_h3(season,part)
+    	break;
+    case 7:video("") 
+smn_h3(season,part)
+    	break;
+    case 8:video("") 
+smn_h3(season,part)
+    	break;
+    case 9:video("") 
+smn_h3(season,part)
+    	break;
+    case 10:video("") 
+smn_h3(season,part)
+    	break;
+    case 11:video("") 
+smn_h3(season,part)
+    	break;
+    case 12:video("") 
+smn_h3(season,part)
+    	break;
+    case 13:video("") 
+smn_h3(season,part)
+    	break;
+ 	}//集数
+    	break;
+ //2季
+ 		case 2:switch (part){
+    case 1:video("https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/917ba7bd5285890812359899391/DIL5tMacW54A.mp4") 
+smn_h3(season,part)
+        break;
+    case 2:video("https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/45d870395285890812491498944/sj8EvieKEj0A.mp4") 
+smn_h3(season,part)
+        break;
+    case 3:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/c088dee95285890812906503751/LHDQaglFGOgA.mp4") 
+smn_h3(season,part)
+   	 break;
+    case 4:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/0df56a605285890813283400306/klCbpegzqMAA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 5:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/e6fda1665285890813625003079/YDgMRoo3C2kA.mp4") 
+smn_h3(season,part)
+        break;
+    case 6:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/5028803a5285890813959859421/Gb6Ybs2c5U8A.mp4") 
+smn_h3(season,part)
+    	break;
+    case 7:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/c7ef0f7a5285890814282586929/M6RGEbsXRsAA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 8:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/73e7e23a5285890814576868920/gYJqASmz7eIA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 9:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/59fa22de5285890814888977404/AN4FElzkuTUA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 10:video("https://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/e5dae5485285890815207345812/JciCWQhKwYEA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 11:video("https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/7b72cbd05285890815430498246/CrHD8QH7oaMA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 12:video("https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/eb60e63d5285890815747954692/5Ycx8SFUhpYA.mp4") 
+smn_h3(season,part)
+    	break;
+    case 13:video("https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/aae8c1ad5285890816071405892/uN6zNFPzFQcA.mp4") 
+smn_h3(season,part)
+    	break;
+ 	}//集数
+        break;
+ 	}//季数
 } 
-//12集
-function smn_2_12()
-{ 
-  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/eb60e63d5285890815747954692/5Ycx8SFUhpYA.mp4";
-  document.getElementById("video").load();
-  smn(2,12)
-} 
-//13集
-function smn_2_13()
-{ 
-  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/aae8c1ad5285890816071405892/uN6zNFPzFQcA.mp4";
-  document.getElementById("video").load();
-  smn(2,13)
-} 
+//------------------------
