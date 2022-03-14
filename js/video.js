5c315abb5f0b8ab0ec224adbd8a971f74431b5a4
Creation·Wong <85749931+CreationWong@users.noreply.github.com>
2022-03-14 22:03:54 +0800

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,19 @@
+//11集
+function smn_2_11()
+{ 
+  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/7b72cbd05285890815430498246/CrHD8QH7oaMA.mp4";
+  document.getElementById("video").load();
+} 
+//12集
+function smn_2_12()
+{ 
+  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/eb60e63d5285890815747954692/5Ycx8SFUhpYA.mp4";
+  document.getElementById("video").load();
+} 
+//13集
+function smn_2_13()
+{ 
+  document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/aae8c1ad5285890816071405892/uN6zNFPzFQcA.mp4";
+  document.getElementById("video").load();
+} 
+//赛马娘2 end
