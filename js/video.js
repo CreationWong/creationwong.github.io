359374bda5a4e0542cd2641a7d6c1500b4c6ea89
Creation·Wong <85749931+CreationWong@users.noreply.github.com>
2022-03-14 23:37:02 +0800

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,19 +1,28 @@
+//------------------------
+//赛马娘标题模板
+function smn(part,part_1)
+{ 
+  document.getElementById("now").innerHTML="赛马娘 Pretty Derby 第"+part+"季 第"+part_1+"集";
+} 
+//------------------------
 //11集
 function smn_2_11()
 { 
   document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/7b72cbd05285890815430498246/CrHD8QH7oaMA.mp4";
   document.getElementById("video").load();
+  smn(2,11)
 } 
 //12集
 function smn_2_12()
 { 
   document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/eb60e63d5285890815747954692/5Ycx8SFUhpYA.mp4";
   document.getElementById("video").load();
+  smn(2,12)
 } 
 //13集
 function smn_2_13()
 { 
   document.getElementById("mp4_src").src="https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/aae8c1ad5285890816071405892/uN6zNFPzFQcA.mp4";
   document.getElementById("video").load();
+  smn(2,13)
 } 
-//赛马娘2 end
