364313fc9764d33e3b09085186b4e9f368303264
Creation·Wong <85749931+CreationWong@users.noreply.github.com>
2022-03-19 21:13:57 +0800

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,58 +1,59 @@
-//视频提交
+//视频流提交
 function video(url)
 { 
   document.getElementById("mp4_src").src=url;
   document.getElementById("video").load();
 } 
 //------------------------
-//赛马娘标题模板
+//赛马娘标题
 function smn_h3(season,part)
 { 
   document.getElementById("now").innerHTML="赛马娘 Pretty Derby 第"+season+"季 第"+part+"集";
+  document.getElementById("now_up").innerHTML="正在播放："+"赛马娘 Pretty Derby 第"+season+"季 第"+part+"集";
 } 
 //------------------------
-//赛马娘季集判断
+//赛马娘季集
 function smn(season,part)
 { 
 	switch (season){
   		case 1:switch (part){
-    case 1:video("") 
+    case 1:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/aa4fd8e25285890810879943779/Bi6MgQiSgAoA.mp4") 
 smn_h3(season,part)
         break;
-    case 2:video("") 
+    case 2:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a7c717bd5285890810879818467/19B5fo49it0A.mp4") 
 smn_h3(season,part)
         break;
-    case 3:video("") 
+    case 3:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a7c71fb65285890810879818622/saMDAwTMGrkA.mp4") 
 smn_h3(season,part)
    	 break;
-    case 4:video("") 
+    case 4:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a85648025285890810879895890/gHXxUBodv7kA.mp4") 
 smn_h3(season,part)
     	break;
-    case 5:video("") 
+    case 5:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/aa50585e5285890810879944468/UavaknGe6v4A.mp4") 
 smn_h3(season,part)
         break;
-    case 6:video("") 
+    case 6:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a856ba5b5285890810879896252/45VcXj4Ng7MA.mp4") 
 smn_h3(season,part)
     	break;
-    case 7:video("") 
+    case 7:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/aa506de55285890810879944988/v4jDNebASegA.mp4") 
 smn_h3(season,part)
     	break;
-    case 8:video("") 
+    case 8:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/aa50d83b5285890810879945185/9qUnJANypv4A.mp4") 
 smn_h3(season,part)
     	break;
-    case 9:video("") 
+    case 9:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a7c7b9005285890810879819937/U0s4829W14AA.mp4") 
 smn_h3(season,part)
     	break;
-    case 10:video("") 
+    case 10:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a85743065285890810879897199/WBFa9pvEIaoA.mp4") 
 smn_h3(season,part)
     	break;
-    case 11:video("") 
+    case 11:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a7d4d0d35285890810879820518/hNC5IJ2EaUMA.mp4") 
 smn_h3(season,part)
     	break;
-    case 12:video("") 
+    case 12:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/a7d4d92c5285890810879820700/aqHIbdx7gk8A.mp4") 
 smn_h3(season,part)
     	break;
-    case 13:video("") 
+    case 13:video("http://1251316161.vod2.myqcloud.com/5f6ddb64vodsh1251316161/aa516c775285890810879946333/MXe5JDesHvgA.mp4") 
 smn_h3(season,part)
     	break;
  	}//集数
@@ -103,3 +104,52 @@ smn_h3(season,part)
  	}//季数
 } 
 //------------------------
+//GJ标题
+function gj_h3(part)
+{ 
+  document.getElementById("now").innerHTML="GJ部 "+"第"+part+"集";
+  document.getElementById("now_up").innerHTML="正在播放："+"GJ部 "+"第"+part+"集";
+} 
+//------------------------
+//GJ季集
+function gj(part)
+{ 
+switch (part){
+    case 1:video("https://upos-sz-mirrorkodo.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708268&gen=playurlv2&os=kodobv&oi=1866711548&trid=891a6e533b2c4ec091ffc0afbe9014f6T&platform=html5&upsig=0752f10391a257ae381d7a413dec00f4&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+        break;
+    case 2:video("https://upos-sz-mirrorkodo.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708372&gen=playurlv2&os=kodobv&oi=1866711548&trid=eb2aaed92804447a9018cbf5a15338fdT&platform=html5&upsig=9609566a04d34b8915b40f20faad1f70&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+        break;
+    case 3:video("https://cn-sdyt-cu-v-02.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708602&gen=playurlv2&os=bcache&oi=1866711548&trid=00007c585cd059c84edd8b6805fa238da095T&platform=html5&upsig=2b79244ee8e2f2d3626d7627ff442fc2&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=1760&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+   	 break;
+    case 4:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708517&gen=playurlv2&os=bcache&oi=1866711548&trid=00007577c5797ac5430cb71fca469e581ec2T&platform=html5&upsig=f5a174874c052b8aec33af1165f27de6&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 5:video("https://cn-lnsy-cu-v-06.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708723&gen=playurlv2&os=bcache&oi=1866711548&trid=0000fc762fa8d0c846a7aab1366c73f93ea3T&platform=html5&upsig=b0c730c81afe08f80c299c96ac343260&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=3285&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+        break;
+    case 6:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708768&gen=playurlv2&os=bcache&oi=1866711548&trid=00007ba4e39841714cc891c08ee45a1967bbT&platform=html5&upsig=1065df41cf9dfbcaedea9f5c21177d40&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 7:video("https://cn-sdyt-cu-v-02.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708803&gen=playurlv2&os=bcache&oi=1866711548&trid=000001d667062e8948a68c04bebb203d1b81T&platform=html5&upsig=66efcd80d12ef8a4d4b3880cbd2973be&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=1760&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 8:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708839&gen=playurlv2&os=bcache&oi=1866711548&trid=0000a09f406abeab4fef98bd2d4711bedaeeT&platform=html5&upsig=56d5c15e537e7b508b01ddc607e15ff3&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 9:video("https://cn-lnsy-cu-v-06.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708871&gen=playurlv2&os=bcache&oi=1866711548&trid=0000d9e20c2740dd4e2499673a6fadde7024T&platform=html5&upsig=b3315dbc6769f4c2d5eaa0dd7b8df5ae&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=3285&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 10:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708907&gen=playurlv2&os=bcache&oi=1866711548&trid=000013a1c620fc7243e8b90e94c578287a1bT&platform=html5&upsig=197ae5357b5d4e6ed46b0de86de2a7c4&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 11:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708935&gen=playurlv2&os=bcache&oi=1866711548&trid=0000a2fe591983144dca9b1b90bcb7513a0fT&platform=html5&upsig=50f19430f731a3dabe5d57ff59d8b0ca&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+    case 12:video("https://cn-nmghhht-cu-v-01.bilivideo.com/upgcxcode/65/56/8705665/8705665-1-208.mp4?e=ig8euxZM2rNcNbRBnWdVhwdlhWUHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1647708969&gen=playurlv2&os=bcache&oi=1866711548&trid=00001467fd125bda49b480640add3cc49e6fT&platform=html5&upsig=a28ebf5c2a9443d86257f2c689edaa6a&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=5240&mid=0&bvc=vod&nettype=0&bw=159248&orderid=0,1&logo=80000000") 
+gj_h3(part)
+    	break;
+ 	}
+}
